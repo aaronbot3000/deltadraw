d50b0a42217c0c8cbaec6cbf429c7bbe8a76519e
Aaron Fan <aaron.y.fan@gmail.com>
2011-06-18 12:21:57 -0500

    Formatting on long equations

--- sympy_scripts/inv_kinematics.py
+++ sympy_scripts/inv_kinematics.py
@@ -36,9 +36,11 @@ rot_matrix = Matrix([(cos(arm_theta), -sin(arm_theta)), (sin(arm_theta), cos(arm
 dist = sqrt(tx**2 + ty**2 + tz**2)
 alpha = (ll**2 - ul**2 - dist**2) / 2
 
-jx1 = (sqrt(tx**2 * tz**2 * ul**2 + tz**4 * ul**2 - tz**2 * alpha**2) - tx * alpha)/(tx**2 + tz**2)
+jx1 = (sqrt(tx**2 * tz**2 * ul**2 + tz**4 * ul**2 - tz**2 * alpha**2) 
+        - tx * alpha)/(tx**2 + tz**2)
 
-jx2 = (-sqrt(tx**2 * tz**2 * ul**2 + tz**4 * ul**2 - tz**2 * alpha**2) - tx * alpha)/(tx**2 + tz**2)
+jx2 = (-sqrt(tx**2 * tz**2 * ul**2 + tz**4 * ul**2 - tz**2 * alpha**2) 
+        - tx * alpha)/(tx**2 + tz**2)
 
 jz1 = acos(jx1/ul)
 jz2 = acos(jx2/ul)
